6bbfed4072f43c5789ad489fc4a2ad120874e70c
Ch4nYH <hfercxx@hotmail.com>
2021-07-15 20:16:52 +0800

    update

--- src/gpt2_ft.py
+++ src/gpt2_ft.py
@@ -215,8 +215,8 @@ def train_validate(model, optimizer, scheduler, train_loader, valid_loader, args
     # evaluation interval
     if train_step % args.eval_interval == 0:
       eval_start_time = time.time()
-
-      #valid_loss, valid_ppl = evaluate(model, valid_loader, args)
+      '''
+      valid_loss, valid_ppl = evaluate(model, valid_loader, args)
 
       if best_val_ppl is None or valid_ppl < best_val_ppl:
         best_val_ppl = valid_ppl
@@ -230,6 +230,7 @@ def train_validate(model, optimizer, scheduler, train_loader, valid_loader, args
         print('-' * 100)
         print(log_str)
         print('-' * 100)
+      '''
       print(grad_tensor_dict)
       for name in grad_tensor_dict.keys():
         param_list = name.split(".")
