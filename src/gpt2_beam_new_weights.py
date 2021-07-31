1877187082d116e805c8abcd3f0bd41b7310788c
Ch4nYH <hfercxx@hotmail.com>
2021-07-31 21:26:49 +0800

    update

--- src/gpt2_beam_new_weights.py
+++ src/gpt2_beam_new_weights.py
@@ -376,7 +376,7 @@ if __name__ == '__main__':
   state = torch.load("lora_weights.pt", map_location="cpu")
   for name, p in lm_net.named_parameters():
       if 'adapter' in name:
-          print(name)
+          print(state.keys())
           p.data = state[name[12:]].to(p.device)
   print('model sampling ...')
   beam(lm_net, valid_loader, args)
