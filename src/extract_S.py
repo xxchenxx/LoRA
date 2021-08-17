36e6f2daefe1d5db3ce293c6d2dda1dba32f45f8
Ch4nYH <hfercxx@hotmail.com>
2021-08-17 17:20:02 +0800

    update

--- src/extract_S.py
+++ src/extract_S.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 checkpoint = torch.load("./trained_models/GPT2_M_compress/e2e/model.2000.pt", map_location="cpu")['model_state_dict']
 
@@ -12,6 +13,15 @@ for key in checkpoint:
         print(checkpoint[key].abs().mean())
 
         if torch.isnan(checkpoint[key].abs().mean()):
-            print(checkpoint[key])
+            mask = [1] * 128
+            mask.extend([0] * (1024 * 1024 - 128))
+            mask = np.array(mask)
+            mask = torch.from_numpy(np.permutation(mask))
+            mask = mask.view(*checkpoint[key].shape)
+        else:
+            threshold, _ = torch.kthvalue(checkpoint[key].view(-1).detach().cpu().numpy(), 1024 * 1024 - 128)
+            mask = (checkpoint[key] >= threshold).long()
+
+        extracted[key] = mask
 
 torch.save(extracted, "extracted_S.pth.tar")
