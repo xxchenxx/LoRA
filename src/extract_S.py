02d628daf48b7a72cc3e9aa5a5becf9048360c18
Ch4nYH <hfercxx@hotmail.com>
2021-08-17 17:20:46 +0800

    update

--- src/extract_S.py
+++ src/extract_S.py
@@ -16,7 +16,7 @@ for key in checkpoint:
             mask = [1] * 128
             mask.extend([0] * (1024 * 1024 - 128))
             mask = np.array(mask)
-            mask = torch.from_numpy(np.permutation(mask))
+            mask = torch.from_numpy(np.random.permutation(mask))
             mask = mask.view(*checkpoint[key].shape)
         else:
             threshold, _ = torch.kthvalue(checkpoint[key].view(-1).detach().cpu(), 1024 * 1024 - 128)
