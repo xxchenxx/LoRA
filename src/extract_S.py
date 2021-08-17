1195e409454582ea81304689aaecf2369e53d76f
Ch4nYH <hfercxx@hotmail.com>
2021-08-17 16:10:45 +0800

    update

--- src/extract_S.py
+++ src/extract_S.py
@@ -8,6 +8,7 @@ extracted = {}
 for key in checkpoint:
     if 'S_Q' or 'S_V' in key:
         extracted[key] = checkpoint[key]
-        print(checkpoint[key])
+        print(checkpoint.mean())
+        print((checkpoint.abs() == 0).float().mean())
 
 torch.save(extracted, "extracted_S.pth.ptar")
