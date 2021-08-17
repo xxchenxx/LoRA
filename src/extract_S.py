a598de5315c4b22dbf7d284cc6494a09c59ca455
Ch4nYH <hfercxx@hotmail.com>
2021-08-17 16:01:28 +0800

    update

--- src/extract_S.py
+++ src/extract_S.py
@@ -1,5 +1,5 @@
 import torch
 
-checkpoint = torch.load("./trained_models/GPT2_M_compress/e2e/model.2000.pt")
+checkpoint = torch.load("./trained_models/GPT2_M_compress/e2e/model.2000.pt")['model_state_dict']
 
 print(checkpoint.keys())
