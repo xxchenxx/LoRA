612a28522aee13852d1a863f31c99ce74c95940a
Ch4nYH <hfercxx@hotmail.com>
2021-08-17 16:09:34 +0800

    update

--- src/extract_S.py
+++ src/extract_S.py
@@ -1,5 +1,12 @@
 import torch
 
-checkpoint = torch.load("./trained_models/GPT2_M_compress/e2e/model.2000.pt")['model_state_dict']
+checkpoint = torch.load("./trained_models/GPT2_M_compress/e2e/model.2000.pt", map_location="cpu")['model_state_dict']
 
 print(checkpoint.keys())
+
+extracted = {}
+for key in checkpoint:
+    if 'S_Q' or 'S_V' in key:
+        extracted[key] = checkpoint[key]
+
+torch.save(extracted, "extracted_S.pth.ptar")
