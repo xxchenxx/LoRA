aca25db8493fced0d44eb9283dfb07ddf53c9193
Ch4nYH <hfercxx@hotmail.com>
2021-08-19 08:47:20 +0800

    updtae

---
+++ src/scripts/0818_eval_random_compress.sh
@@ -0,0 +1,19 @@
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 src/gpt2_beam.py \
+    --data ./data/e2e/test.jsonl \
+    --batch_size 1 \
+    --seq_len 512 \
+    --eval_len 64 \
+    --model_card gpt2.md \
+    --init_checkpoint ./trained_models/GPT2_M_random_compress_train/e2e/model.50000.pt \
+    --platform local \
+    --lora_dim 2 \
+    --lora_alpha 32 \
+    --beam 10 \
+    --length_penalty 0.8 \
+    --no_repeat_ngram_size 4 \
+    --repetition_penalty 1.0 \
+    --eos_token_id 628 \
+    --work_dir ./trained_models/GPT2_M_random_compress_train/e2e \
+    --output_file predict.50000.b10p08.jsonl
+
+
