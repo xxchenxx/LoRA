d061133c4ed0ac9d701fcfa097b0fd75bab10aa7
Ch4nYH <hfercxx@hotmail.com>
2021-08-27 23:05:00 +0800

    update

---
+++ src/scripts/0821_train.sh
@@ -0,0 +1,63 @@
+nohup python -m torch.distributed.launch --nproc_per_node=4 --master_port=5678 src/gpt2_ft.py \
+    --train_data ./data/dart/train.jsonl \
+    --valid_data ./data/dart/valid.jsonl \
+    --train_batch_size 2 \
+    --grad_acc 1 \
+    --valid_batch_size 1 \
+    --seq_len 512 \
+    --model_card gpt2.md \
+    --init_checkpoint ./pretrained_checkpoints/gpt2-medium-pytorch_model.bin \
+    --platform local \
+    --clip 0.0 \
+    --lr 0.0002 \
+    --weight_decay 0.01 \
+    --correct_bias \
+    --adam_beta2 0.999 \
+    --scheduler linear \
+    --warmup_step 500 \
+    --max_epoch 5 \
+    --save_interval 1000 \
+    --lora_dim 2 \
+    --lora_alpha 32 \
+    --lora_dropout 0.1 \
+    --label_smooth 0.1 \
+    --work_dir ./trained_models/GPT2_M/dart \
+    --random_seed 110 \
+    --save_interval 10000 > 0822_train_lora_2.out &
+
+
+CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch --nproc_per_node=2 --master_port=1234 src/gpt2_beam.py \
+    --data ./data/dart/test.jsonl \
+    --batch_size 1 \
+    --seq_len 512 \
+    --eval_len 64 \
+    --model_card gpt2.md \
+    --init_checkpoint ./trained_models/GPT2_M/dart/model.39160.pt \
+    --platform local \
+    --lora_dim 3 \
+    --lora_alpha 32 \
+    --beam 10 \
+    --length_penalty 0.8 \
+    --no_repeat_ngram_size 4 \
+    --repetition_penalty 1.0 \
+    --eos_token_id 628 \
+    --work_dir ./trained_models/GPT2_M/dart \
+    --output_file predict.39160.b10p08.jsonl
+
+python src/gpt2_decode.py \
+        --vocab ./vocab \
+        --sample_file ./trained_models/GPT2_M/dart/predict.39160.b10p08.jsonl \
+        --input_file ./data/dart/test_formatted.jsonl \
+        --ref_type dart \
+        --ref_num 6 \
+        --output_ref_file eval/GenerationEval/data/references_dart \
+        --output_pred_file eval/GenerationEval/data/hypothesis_dart \
+        --tokenize --lower
+
+cd ./eval/GenerationEval/
+python eval.py \
+    -R data/references_dart/reference \
+    -H data/hypothesis_dart \
+    -nr 6 \
+    -m bleu,meteor,ter 
+cd ../..
