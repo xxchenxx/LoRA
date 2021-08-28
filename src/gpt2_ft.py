7b11fb8c2139a309aa979948d98213a9644c05cf
Ch4nYH <hfercxx@hotmail.com>
2021-08-28 11:26:01 +0800

    update

--- src/gpt2_ft.py
+++ src/gpt2_ft.py
@@ -301,20 +301,20 @@ if __name__ == '__main__':
         Q_weight = module.c_attn.weight[:, :module.split_size]
         V_weight = module.c_attn.weight[:, 2*module.split_size:]
 
-        U_Q = torch.qr((Q_weight - module.S_Q) @ module.q_proj_adapter1.data.T)[0]
-        U_Q_change.append(torch.norm(U_Q - module.q_proj_adapter2.data))
-        module.q_proj_adapter2.data = U_Q
+        U_Q = torch.qr((Q_weight - module.S_Q) @ module.q_proj_adapter1.weight.data.T)[0]
+        U_Q_change.append(torch.norm(U_Q - module.q_proj_adapter2.weight.data))
+        module.q_proj_adapter2.weight.data = U_Q
         V_Q = U_Q.T @ (Q_weight - module.S_Q.data)
-        module.q_proj_adapter1.data = V_Q
+        module.q_proj_adapter1.weight.data = V_Q
         module.S_Q = U_Q @ V_Q
 
         q, _ = torch.kthvalue(module.S_Q.abs().view(-1), module.S_Q.numel() - 128)
         module.S_Q[module.S_Q.abs() < q] = 0
 
-        U_V = torch.qr((V_weight - module.S_V) @ module.v_proj_adapter1.data.T)[0]
-        module.v_proj_adapter2.data = U_V
+        U_V = torch.qr((V_weight - module.S_V) @ module.v_proj_adapter1.weight.data.T)[0]
+        module.v_proj_adapter2.weight.data = U_V
         V_V = U_V.T @ (Q_weight - module.S_V.data)
-        module.v_proj_adapter1.data = V_V
+        module.v_proj_adapter1.weight.data = V_V
         module.S_V = U_V @ V_V
 
         v, _ = torch.kthvalue(module.S_V.abs().view(-1), module.S_V.numel() - 128)
