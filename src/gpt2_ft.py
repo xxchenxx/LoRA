bec49860d4a3ac8cc53cc0d5aafcb77a0d8944a0
Ch4nYH <hfercxx@hotmail.com>
2021-09-01 00:35:42 +0800

    update

--- src/gpt2_ft.py
+++ src/gpt2_ft.py
@@ -90,8 +90,8 @@ def print_args(args):
 def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    #torch.backends.cudnn.deterministic = True
-    #torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
     np.random.seed(seed)
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
