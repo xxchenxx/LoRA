29864023ef1088065b381c15bb9b1f4ca442c1cd
Ch4nYH <hfercxx@hotmail.com>
2021-09-03 00:13:44 +0800

    updaaate

--- src/scripts/0902_eval_vm1.sh
+++ src/scripts/0902_eval_vm1.sh
@@ -1,4 +1,4 @@
-python src/gpt2_decode.py          --vocab ./vocab          --sample_file ./trained_models/GPT2_M/dart_rank2_bilateral_step1000/predict.100000.b10p08.jsonl          --input_file ./data/dart/test_formatted.jsonl          --ref_type dart          --ref_num 6          --output_ref_file eval/GenerationEval/data/references_dart          --output_pred_file eval/GenerationEval/data/hypothesis_dart          --tokenize --lower
+python src/gpt2_decode.py          --vocab ./vocab          --sample_file ./trained_models/GPT2_M/dart_rank3_bilateral_step1000/predict.100000.b10p08.jsonl          --input_file ./data/dart/test_formatted.jsonl          --ref_type dart          --ref_num 6          --output_ref_file eval/GenerationEval/data/references_dart          --output_pred_file eval/GenerationEval/data/hypothesis_dart          --tokenize --lower
 
 cd ./eval/GenerationEval/
 python eval.py      -R data/references_dart/reference      -H data/hypothesis_dart      -nr 6      -m bleu,meteor,ter 
