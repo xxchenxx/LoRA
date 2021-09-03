73ffaa69def115afbaf3ed5a31be30ff68cf2e0c
Ch4nYH <hfercxx@hotmail.com>
2021-09-03 09:13:49 +0800

    update

--- src/analysis.py
+++ src/analysis.py
@@ -1,13 +1,12 @@
 import torch
-
-a = torch.load('./trained_models/GPT2_M_bilateral_rank1_1000/e2e/model.105155.pt',map_location='cpu')['model_state_dict']
-b = torch.load('./trained_models/GPT2_M_bilateral_rank1_500/e2e/model.105155.pt',map_location='cpu')['model_state_dict']
+a = torch.load("./trained_models/GPT2_M_bilateral_rank1_1/e2e/model.105155.pt", map_location='cpu')
+['model_state_dict']
+b = torch.load("./trained_models/GPT2_M_bilateral_rank1_10/e2e/model.105155.pt", map_location='cpu')
+['model_state_dict']
 
 for key in a:
-    if ('S_Q' in key or 'S_V' in key) and ('embedding' not in key):
+    if 'S_V_embedding' in key:
         print(key)
-        print(a[key].sum())
-        print(b[key].sum())
+        print(a[key])
+        print(b[key])
         print((a[key] * b[key]).sum())
-        print('----')
-        
