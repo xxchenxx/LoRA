939b718a49d18860f1193143f0402619b7732806
Ch4nYH <hfercxx@hotmail.com>
2021-09-06 10:11:57 +0800

    update

---
+++ src/scripts/0901_eval_ut_vita3.sh
@@ -0,0 +1,6 @@
+python src/gpt2_decode.py \
+    --vocab ./vocab \
+    --sample_file ./trained_models/GPT2_M_rank4_seed1/e2e/predict.105155.b10p08.jsonl \
+    --input_file ./data/e2e/test_formatted.jsonl \
+    --output_ref_file e2e_ref.txt \
+    --output_pred_file e2e_pred.txt
