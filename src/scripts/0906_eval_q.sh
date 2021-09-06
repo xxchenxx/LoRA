3acf5a0bb48fdaaee51358c24f06c1993f0c3c02
Ch4nYH <hfercxx@hotmail.com>
2021-09-06 10:20:04 +0800

    update

---
+++ src/scripts/0906_eval_q.sh
@@ -0,0 +1,68 @@
+cp eval/eval.py eval/GenerationEval/
+python src/gpt2_decode.py \
+    --vocab ./vocab \
+    --sample_file ./trained_models/GPT2_M/webnlg_challenge_2017_rank1_bilateral_step100/predict.45065.b10p08.jsonl \
+    --input_file ./data/webnlg_challenge_2017/test_formatted.jsonl \
+    --ref_type webnlg \
+    --ref_num 6 \
+    --output_ref_file eval/GenerationEval/data/references_webnlg \
+    --output_pred_file eval/GenerationEval/data/hypothesis_webnlg \
+    --tokenize --lower
+cd ./eval/GenerationEval/
+python eval.py \
+    -R data/references_webnlg/reference \
+    -H data/hypothesis_webnlg \
+    -nr 6 \
+    -m bleu,meteor,ter >> 0906_eval.out
+cd ../..
+
+python src/gpt2_decode.py \
+    --vocab ./vocab \
+    --sample_file ./trained_models/GPT2_M/webnlg_challenge_2017_rank2_bilateral_step100/predict.45065.b10p08.jsonl \
+    --input_file ./data/webnlg_challenge_2017/test_formatted.jsonl \
+    --ref_type webnlg \
+    --ref_num 6 \
+    --output_ref_file eval/GenerationEval/data/references_webnlg \
+    --output_pred_file eval/GenerationEval/data/hypothesis_webnlg \
+    --tokenize --lower
+cd ./eval/GenerationEval/
+python eval.py \
+    -R data/references_webnlg/reference \
+    -H data/hypothesis_webnlg \
+    -nr 6 \
+    -m bleu,meteor,ter >> 0906_eval.out
+cd ../..
+
+python src/gpt2_decode.py \
+    --vocab ./vocab \
+    --sample_file ./trained_models/GPT2_M/webnlg_challenge_2017_rank3_bilateral_step100/predict.45065.b10p08.jsonl \
+    --input_file ./data/webnlg_challenge_2017/test_formatted.jsonl \
+    --ref_type webnlg \
+    --ref_num 6 \
+    --output_ref_file eval/GenerationEval/data/references_webnlg \
+    --output_pred_file eval/GenerationEval/data/hypothesis_webnlg \
+    --tokenize --lower
+cd ./eval/GenerationEval/
+python eval.py \
+    -R data/references_webnlg/reference \
+    -H data/hypothesis_webnlg \
+    -nr 6 \
+    -m bleu,meteor,ter >> 0906_eval.out
+cd ../..
+
+python src/gpt2_decode.py \
+    --vocab ./vocab \
+    --sample_file ./trained_models/GPT2_M/webnlg_challenge_2017_rank4_bilateral_step100/predict.45065.b10p08.jsonl \
+    --input_file ./data/webnlg_challenge_2017/test_formatted.jsonl \
+    --ref_type webnlg \
+    --ref_num 6 \
+    --output_ref_file eval/GenerationEval/data/references_webnlg \
+    --output_pred_file eval/GenerationEval/data/hypothesis_webnlg \
+    --tokenize --lower
+cd ./eval/GenerationEval/
+python eval.py \
+    -R data/references_webnlg/reference \
+    -H data/hypothesis_webnlg \
+    -nr 6 \
+    -m bleu,meteor,ter >> 0906_eval.out
+cd ../..
