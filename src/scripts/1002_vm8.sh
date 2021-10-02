ceffd9961f7941baea55312c3b53dfc07937911b
Ch4nYH <hfercxx@hotmail.com>
2021-10-02 08:51:44 +0800

    update

--- src/scripts/1002_vm8.sh
+++ src/scripts/1002_vm8.sh
@@ -4,7 +4,7 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_pe
     --seq_len 512 \
     --eval_len 64 \
     --model_card gpt2.md \
-    --init_checkpoint ./trained_models/GPT2_M_bilateral_smooth_rank32_seed110/model.26289.pt \
+    --init_checkpoint ./trained_models/GPT2_M_bilateral_smooth_rank32_seed110/e2e/model.26289.pt \
     --platform local \
     --lora_dim 32 \
     --lora_alpha 32 \
@@ -13,7 +13,7 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_pe
     --no_repeat_ngram_size 4 \
     --repetition_penalty 1.0 \
     --eos_token_id 628 \
-    --work_dir ./trained_models/GPT2_M_bilateral_smooth_rank32_seed110 \
+    --work_dir ./trained_models/GPT2_M_bilateral_smooth_rank32_seed110/e2e \
     --output_file predict.26289.b10p08.jsonl &
 
 
@@ -23,7 +23,7 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_pe
     --seq_len 512 \
     --eval_len 64 \
     --model_card gpt2.md \
-    --init_checkpoint ./trained_models/GPT2_M_bilateral_smooth_rank16_seed110/model.26289.pt \
+    --init_checkpoint ./trained_models/GPT2_M_bilateral_smooth_rank16_seed110/e2e/model.26289.pt \
     --platform local \
     --lora_dim 16 \
     --lora_alpha 32 \
@@ -32,7 +32,7 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_pe
     --no_repeat_ngram_size 4 \
     --repetition_penalty 1.0 \
     --eos_token_id 628 \
-    --work_dir ./trained_models/GPT2_M_bilateral_smooth_rank16_seed110 \
+    --work_dir ./trained_models/GPT2_M_bilateral_smooth_rank16_seed110/e2e \
     --output_file predict.26289.b10p08.jsonl &
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port=8763 src/gpt2_beam.py \
@@ -41,7 +41,7 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_pe
     --seq_len 512 \
     --eval_len 64 \
     --model_card gpt2.md \
-    --init_checkpoint ./trained_models/GPT2_M_bilateral_smooth_rank8_seed110/model.26289.pt \
+    --init_checkpoint ./trained_models/GPT2_M_bilateral_smooth_rank8_seed110/e2e/model.26289.pt \
     --platform local \
     --lora_dim 8 \
     --lora_alpha 32 \
@@ -50,7 +50,7 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_pe
     --no_repeat_ngram_size 4 \
     --repetition_penalty 1.0 \
     --eos_token_id 628 \
-    --work_dir ./trained_models/GPT2_M_bilateral_smooth_rank8_seed110 \
+    --work_dir ./trained_models/GPT2_M_bilateral_smooth_rank8_seed110/e2e \
     --output_file predict.26289.b10p08.jsonl
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port=8764 src/gpt2_beam.py \
@@ -59,7 +59,7 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_pe
     --seq_len 512 \
     --eval_len 64 \
     --model_card gpt2.md \
-    --init_checkpoint ./trained_models/GPT2_M_bilateral_smooth_rank6_seed110/model.26289.pt \
+    --init_checkpoint ./trained_models/GPT2_M_bilateral_smooth_rank6_seed110/e2e/model.26289.pt \
     --platform local \
     --lora_dim 6 \
     --lora_alpha 32 \
@@ -68,7 +68,7 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_pe
     --no_repeat_ngram_size 4 \
     --repetition_penalty 1.0 \
     --eos_token_id 628 \
-    --work_dir ./trained_models/GPT2_M_bilateral_smooth_rank6_seed110 \
+    --work_dir ./trained_models/GPT2_M_bilateral_smooth_rank6_seed110/e2e \
     --output_file predict.26289.b10p08.jsonl &
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port=8765 src/gpt2_beam.py \
@@ -77,7 +77,7 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_pe
     --seq_len 512 \
     --eval_len 64 \
     --model_card gpt2.md \
-    --init_checkpoint ./trained_models/GPT2_M_bilateral_smooth_rank4_seed110/model.26289.pt \
+    --init_checkpoint ./trained_models/GPT2_M_bilateral_smooth_rank4_seed110/e2e/model.26289.pt \
     --platform local \
     --lora_dim 4 \
     --lora_alpha 32 \
@@ -86,7 +86,7 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_pe
     --no_repeat_ngram_size 4 \
     --repetition_penalty 1.0 \
     --eos_token_id 628 \
-    --work_dir ./trained_models/GPT2_M_bilateral_smooth_rank4_seed110 \
+    --work_dir ./trained_models/GPT2_M_bilateral_smooth_rank4_seed110/e2e \
     --output_file predict.26289.b10p08.jsonl &
 
 git checkout main
@@ -97,7 +97,7 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_pe
     --seq_len 512 \
     --eval_len 64 \
     --model_card gpt2.md \
-    --init_checkpoint ./trained_models/GPT2_M_rank4_seed110/model.26289.pt \
+    --init_checkpoint ./trained_models/GPT2_M_rank4_seed110/e2e/model.26289.pt \
     --platform local \
     --lora_dim 4 \
     --lora_alpha 32 \
@@ -106,6 +106,6 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python -m torch.distributed.launch --nproc_pe
     --no_repeat_ngram_size 4 \
     --repetition_penalty 1.0 \
     --eos_token_id 628 \
-    --work_dir ./trained_models/GPT2_M_rank4_seed110 \
+    --work_dir ./trained_models/GPT2_M_rank4_seed110/e2e \
     --output_file predict.26289.b10p08.jsonl
 
