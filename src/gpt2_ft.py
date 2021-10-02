c67ec758085130a73594bb27e6f580b01bae7b1f
Ch4nYH <hfercxx@hotmail.com>
2021-10-03 01:23:43 +0800

    update

--- src/gpt2_ft.py
+++ src/gpt2_ft.py
@@ -313,15 +313,15 @@ if __name__ == '__main__':
   for name, module in lm_net.named_modules():
     residual_change = []
     if isinstance(module, Attention):
-      Q_weight = module.c_attn.weight[:, :module.split_size].detach().cpu()
-      V_weight = module.c_attn.weight[:, 2*module.split_size:].detach().cpu()
-      U_Q = torch.randn((module.q_proj_adapter2.weight.data.shape[0], 1)).to(Q_weight.device).detach().cpu()
-      V_Q = torch.randn((1, module.q_proj_adapter1.weight.data.shape[1])).to(Q_weight.device).detach().cpu()
-      S_Q = module.S_Q.data.detach().cpu()
+      Q_weight = module.c_attn.weight[:, :module.split_size]
+      V_weight = module.c_attn.weight[:, 2*module.split_size:]
+      U_Q = torch.randn((module.q_proj_adapter2.weight.data.shape[0], 1)).to(Q_weight.device)
+      V_Q = torch.randn((1, module.q_proj_adapter1.weight.data.shape[1])).to(Q_weight.device)
+      S_Q = module.S_Q.data
 
-      U_V = torch.randn((module.v_proj_adapter2.weight.data.shape[0], 1)).to(Q_weight.device).detach().cpu()
-      V_V = torch.randn((1, module.v_proj_adapter1.weight.data.shape[1])).to(Q_weight.device).detach().cpu()
-      S_V = module.S_V.data.detach().cpu()
+      U_V = torch.randn((module.v_proj_adapter2.weight.data.shape[0], 1)).to(Q_weight.device)
+      V_V = torch.randn((1, module.v_proj_adapter1.weight.data.shape[1])).to(Q_weight.device)
+      S_V = module.S_V.data
       for rank in range(31):
         for _ in range(args.compress_step):
           U_Q = torch.qr((Q_weight - S_Q) @ V_Q.T)[0]
