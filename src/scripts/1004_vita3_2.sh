39f542bf7d085d380858f26d4c94dd8da03f3aed
Ch4nYH <hfercxx@hotmail.com>
2021-10-05 00:48:36 +0800

    update

--- src/scripts/1004_vita3_2.sh
+++ src/scripts/1004_vita3_2.sh
@@ -1,3 +1,43 @@
 NCCL_P2P_DISABLE=1 CUDA_VISIBLE_DEVICES=0 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port=8293 src/gpt2_ft_prune.py --train_data ./data/e2e/train.jsonl --valid_data ./data/e2e/valid.jsonl --train_batch_size 2 --grad_acc 1 --valid_batch_size 1 --seq_len 512 --model_card gpt2.md --init_checkpoint ./trained_models/GPT2_M_e2e_rank_2/model.105155.pt --platform local --clip 0.0 --lr 0.0002 --weight_decay 0.01 --correct_bias --adam_beta2 0.999 --scheduler linear --warmup_step 500 --max_epoch 1 --save_interval 1000 --label_smooth 0.1 --work_dir ./trained_models/GPT2_M_e2e_rank_2_unstructure --lora_dim 2 --lora_alpha 32 --lora_dropout 0.1 --num_sparse 128 --random_seed 110 --save_interval 10000 --pruning_ratio 0.1 &
 
 NCCL_P2P_DISABLE=1 CUDA_VISIBLE_DEVICES=1 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port=8293 src/gpt2_ft_prune.py --train_data ./data/e2e/train.jsonl --valid_data ./data/e2e/valid.jsonl --train_batch_size 2 --grad_acc 1 --valid_batch_size 1 --seq_len 512 --model_card gpt2.md --init_checkpoint ./trained_models/GPT2_M_e2e_rank_2/model.105155.pt --platform local --clip 0.0 --lr 0.0002 --weight_decay 0.01 --correct_bias --adam_beta2 0.999 --scheduler linear --warmup_step 500 --max_epoch 1 --save_interval 1000 --label_smooth 0.1 --work_dir ./trained_models/GPT2_M_e2e_rank_2_unstructure_0.3 --lora_dim 2 --lora_alpha 32 --lora_dropout 0.1 --num_sparse 128 --random_seed 110 --save_interval 10000 --pruning_ratio 0.3 &
+
+NCCL_P2P_DISABLE=1 CUDA_VISIBLE_DEVICES=1 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port=8293 src/gpt2_ft_prune.py --train_data ./data/e2e/train.jsonl --valid_data ./data/e2e/valid.jsonl --train_batch_size 2 --grad_acc 1 --valid_batch_size 1 --seq_len 512 --model_card gpt2.md --init_checkpoint ./trained_models/GPT2_M_e2e_rank_2/model.105155.pt --platform local --clip 0.0 --lr 0.0002 --weight_decay 0.01 --correct_bias --adam_beta2 0.999 --scheduler linear --warmup_step 500 --max_epoch 1 --save_interval 1000 --label_smooth 0.1 --work_dir ./trained_models/GPT2_M_e2e_rank_2_unstructure_0.5 --lora_dim 2 --lora_alpha 32 --lora_dropout 0.1 --num_sparse 128 --random_seed 110 --save_interval 10000 --pruning_ratio 0.5 &
+
+
+
+CUDA_VISIBLE_DEVICES=2 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port=8766 src/gpt2_beam.py \
+    --data ./data/e2e/test.jsonl \
+    --batch_size 1 \
+    --seq_len 512 \
+    --eval_len 64 \
+    --model_card gpt2.md \
+    --init_checkpoint ./trained_models/GPT2_M_e2e_rank_2_unstructure/model.21031.pt \
+    --platform local \
+    --lora_dim 2 \
+    --lora_alpha 32 \
+    --beam 10 \
+    --length_penalty 0.8 \
+    --no_repeat_ngram_size 4 \
+    --repetition_penalty 1.0 \
+    --eos_token_id 628 \
+    --work_dir ./trained_models/GPT2_M_e2e_rank_2_unstructure \
+    --output_file predict.21031.b10p08.jsonl &
+
+CUDA_VISIBLE_DEVICES=3 nohup python -m torch.distributed.launch --nproc_per_node=1 --master_port=8767 src/gpt2_beam.py \
+    --data ./data/e2e/test.jsonl \
+    --batch_size 1 \
+    --seq_len 512 \
+    --eval_len 64 \
+    --model_card gpt2.md \
+    --init_checkpoint ./trained_models/GPT2_M_e2e_rank_2_unstructure_0.3/model.21031.pt \
+    --platform local \
+    --lora_dim 2 \
+    --lora_alpha 32 \
+    --beam 10 \
+    --length_penalty 0.8 \
+    --no_repeat_ngram_size 4 \
+    --repetition_penalty 1.0 \
+    --eos_token_id 628 \
+    --work_dir ./trained_models/GPT2_M_e2e_rank_2_unstructure_0.3 \
+    --output_file predict.21031.b10p08.jsonl &
