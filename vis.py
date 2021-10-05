9bb85238d4a112434be66ebd8e0372cbbe1892b3
Ch4nYH <hfercxx@hotmail.com>
2021-10-06 04:21:54 +0800

    update

--- vis.py
+++ vis.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-init = torch.load("./pretrained_checkpoints/gpt2-medium-pytorch_model.bin", map_location="cpu")['model_state_dict']
+init = torch.load("./pretrained_checkpoints/gpt2-medium-pytorch_model.bin", map_location="cpu")
 trained = torch.load("./trained_models/GPT2_M_original/e2e/model.105155.pt",  map_location="cpu")['model_state_dict']
 
 #diff = []
