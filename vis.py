80c5fc9e131bbc5ff6a9701cd91818519bd9f0ef
Ch4nYH <hfercxx@hotmail.com>
2021-10-06 04:20:55 +0800

    update

---
+++ vis.py
@@ -0,0 +1,14 @@
+import torch
+import numpy as np
+init = torch.load("./pretrained_checkpoints/gpt2-medium-pytorch_model.bin", map_location="cpu")['model_state_dict']
+trained = torch.load("./trained_models/GPT2_M_original/e2e/model.105155.pt",  map_location="cpu")['model_state_dict']
+
+diff = []
+for key in init:
+    diff.append((init[key] - trained[key]).numpy())
+    print(np.abs((init[key] - trained[key]).numpy()))
+
+
+
+
+
