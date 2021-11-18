461aeded82833e727a2c70053d468060231b546e
Ch4nYH <hfercxx@hotmail.com>
2021-11-18 15:25:17 +0800

    update

--- src/gpt2_ft_slimming_train.py
+++ src/gpt2_ft_slimming_train.py
@@ -311,7 +311,19 @@ if __name__ == '__main__':
   try:
     train_step = 0
     block_modules = []
-    slimming_coefs = np.load('self_slimming_coef_records.npy')
+    checkpoint = torch.load(args.init_checkpoint, map_location="cpu")['model_state_dict']
+    
+    slimming_coefs = []
+    count = 0
+    print(checkpoint.keys())
+    for k in checkpoint:
+        if 'slimming_coef' in k:
+            slimming_coefs.append(checkpoint[k].detach().view(-1).numpy())
+            count += 1
+
+    print(count)
+    slimming_coefs = np.stack(slimming_coefs)
+    print(slimming_coefs.shape)
     for m in lm_net.modules():
         if isinstance(m, Block):
             block_modules.append(m)
