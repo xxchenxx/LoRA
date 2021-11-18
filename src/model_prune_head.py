00f9ee47f130a76459f7d078a21401f0b06c4334
Ch4nYH <hfercxx@hotmail.com>
2021-11-18 15:29:45 +0800

    update

--- src/model_prune_head.py
+++ src/model_prune_head.py
@@ -611,7 +611,7 @@ class MLP(nn.Module):
 
         # Prune linear layers
         self.c_fc = prune_conv1d(self.c_fc, index)
-        self.c_proj = prune_conv1d(self.output.dense, index, dim=0)
+        self.c_proj = prune_conv1d(self.c_proj, index, dim=0)
 
         # Update hyper params and store pruned neurons
         self.pruned_inter_neurons = self.pruned_inter_neurons.union(neurons)
